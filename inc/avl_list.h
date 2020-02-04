850fd1feea3db85d10f87939ac82db1eefed57a8
Sergey Pisarev <pisarev.sergey@gmail.com>
2020-02-05 00:03:30 +0300

    avl list update

--- inc/avl_list.h
+++ inc/avl_list.h
@@ -6,10 +6,12 @@ namespace win_kernel_lib
 {
   namespace avl_list_facility
   {
+    using allocate_routine = void* (*)(CLONG size);
+    using free_routine = void (*)(void*);
+
     template <typename T,
-      PRTL_AVL_COMPARE_ROUTINE comp,
-      PRTL_AVL_ALLOCATE_ROUTINE alloc,
-      PRTL_AVL_FREE_ROUTINE free>
+      allocate_routine custom_alloc,
+      free_routine custom_free>
     class avl_list
     {
     public:
@@ -37,6 +39,40 @@ namespace win_kernel_lib
       T* get_element_by_key(void* key_entry)
       {
         return static_cast<T*>(RtlLookupElementGenericTableAvl(&table, key_entry));
+      }
+
+      static void* alloc(RTL_AVL_TABLE*, CLONG size)
+      {
+        return custom_alloc(size);
+      }
+
+      static void free(RTL_AVL_TABLE*, void* p)
+      {
+        T* object{static_cast<char*>(p) + sizeof(RTL_BALANCED_LINKS)};
+        object->~T();
+
+        custom_free(p);
+      }
+
+      RTL_GENERIC_COMPARE_RESULTS comp(RTL_AVL_TABLE*,
+        void* first,
+        void* second)
+      {
+        auto result{ GenericEqual };
+
+        T* first_object{ static_cast<T*>(first) };
+        T* second_object{ static_cast<T*>(second) };
+
+        if (*first_object < *second_object)
+        {
+          result = GenericLessThan;
+        }
+        else if (*first_object > * second_object)
+        {
+          result = GenericGreaterThan;
+        }
+
+        return result;
       }
 
     private:
