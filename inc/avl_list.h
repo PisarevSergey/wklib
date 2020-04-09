705cfaae0c951f6b9e9b352e0c93d4ea6323ef0e
sergey pisarev <pisarev.sergey@gmail.com>
2020-04-09 22:47:11 +0300

    avl list now stores pointers to objects

--- inc/avl_list.h
+++ inc/avl_list.h
@@ -10,9 +10,11 @@ namespace win_kernel_lib
     template <typename T,
       allocate_routine custom_alloc,
       free_routine custom_free>
-    class avl_list
+    class avl_list  //list of pointers to objects
     {
     public:
+      using stored_item_type = T*;
+
       avl_list()
       {
         RtlInitializeGenericTableAvl(&table, comp, alloc, free, nullptr);
@@ -26,17 +28,17 @@ namespace win_kernel_lib
         }
       }
 
-      bool insert(void* item, CLONG item_size)
+      bool insert(stored_item_type item) // copy of pointer to object
       {
         BOOLEAN inserted;
-        RtlInsertElementGenericTableAvl(&table, item, item_size, &inserted);
+        RtlInsertElementGenericTableAvl(&table, &item, sizeof(item), &inserted);
 
-        return (TRUE == inserted);
+        return (inserted ? true : false);
       }
 
-      T* get_element_by_key(void* key_entry)
+      stored_item_type* get_element_by_key(stored_item_type key_entry)
       {
-        return static_cast<T*>(RtlLookupElementGenericTableAvl(&table, key_entry));
+        return static_cast<stored_item_type*>(RtlLookupElementGenericTableAvl(&table, &key_entry));
       }
 
       static void* alloc(RTL_AVL_TABLE*, CLONG size)
@@ -46,8 +48,8 @@ namespace win_kernel_lib
 
       static void free(RTL_AVL_TABLE*, void* p)
       {
-        T* object{static_cast<T*>(Add2Ptr(p, sizeof(RTL_BALANCED_LINKS)))};
-        object->~T();
+        stored_item_type* entry{ static_cast<stored_item_type*>(Add2Ptr(p, sizeof(RTL_BALANCED_LINKS))) };
+        delete *entry;
 
         custom_free(p);
       }
@@ -58,14 +60,14 @@ namespace win_kernel_lib
       {
         auto result{ GenericEqual };
 
-        T* first_object{ static_cast<T*>(first) };
-        T* second_object{ static_cast<T*>(second) };
+        stored_item_type* first_object{ static_cast<stored_item_type*>(first) };
+        stored_item_type* second_object{ static_cast<stored_item_type*>(second) };
 
-        if (*first_object < *second_object)
+        if (**first_object < **second_object)
         {
           result = GenericLessThan;
         }
-        else if (*first_object > * second_object)
+        else if (**first_object > **second_object)
         {
           result = GenericGreaterThan;
         }
@@ -74,12 +76,12 @@ namespace win_kernel_lib
       }
 
     private:
-      T* get_element_by_number(ULONG num)
+      stored_item_type* get_element_by_number(ULONG num)
       {
-        return static_cast<T*>(RtlGetElementGenericTableAvl(&table, num));
+        return static_cast<stored_item_type*>(RtlGetElementGenericTableAvl(&table, num));
       }
 
-      void delete_entry(void* entry)
+      void delete_entry(stored_item_type* entry)
       {
         RtlDeleteElementGenericTableAvl(&table, entry);
       }
