98a2920424aee52bf93aca01b78fe60586c65b5b
sergey pisarev <pisarev.sergey@gmail.com>
2020-04-09 22:48:48 +0300

    pointer to const used in add_to_ptr

--- inc/pointer_mainpulation.h
+++ inc/pointer_mainpulation.h
@@ -7,10 +7,10 @@ namespace win_kernel_lib
     template<typename out_type, typename in_type>
     out_type* add_to_ptr(in_type* base, size_t offset)
     {
-      char* result{reinterpret_cast<char*>(base)};
+      const char* result{reinterpret_cast<const char*>(base)};
       result += offset;
 
-      return reinterpret_cast<out_type*>(result);
+      return reinterpret_cast<out_type*>(const_cast<char*>(result));
     }
   }
 }
